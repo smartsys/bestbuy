39a0679c41f90369d67f52889f5191de2d174fad
Dirkstar <info@smartsys.de>
2026-09-21 17:03:57 +0200

    Store klasse erstellt

---
+++ store.py
@@ -0,0 +1,41 @@
+from typing import List
+
+import product
+
+
+class Store:
+    def __init__(self, product_list):
+        self.products = product_list
+
+    def add_product(self, product):
+        self.products.append(product)
+
+    def remove_product(self, product):
+        self.products.remove(product)
+
+    def get_total_quantity(self) -> int:
+        return sum(product.get_quantity() for product in self.products)
+
+    def get_all_products(self) -> List[product.Product]:
+        return [product for product in self.products if product.is_active()]
+
+    def order(self, shopping_list) -> float:
+        total_price = 0.0
+        for product, quantity in shopping_list:
+            total_price += product.buy(quantity)
+        return total_price
+
+
+def main():
+    product_list = [product.Product("MacBook Air M2", price=1450, quantity=100),
+                    product.Product("Bose QuietComfort Earbuds", price=250, quantity=500),
+                    product.Product("Google Pixel 7", price=500, quantity=250),
+                    ]
+
+    best_buy = Store(product_list)
+    products = best_buy.get_all_products()
+    print(best_buy.get_total_quantity())
+    print(best_buy.order([(products[0], 1), (products[1], 2)]))
+
+if __name__ == "__main__":
+        main()
